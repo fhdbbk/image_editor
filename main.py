e5d1a8cbea70505b8ebe8aa891db6d99969b6971
Fahad <fha0786@gmail.com>
2022-08-11 00:13:42 +0530

    Added menubar

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import sys
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 
-app = QtWidgets.QApplication(sys.argv)
-windows = QtWidgets.QWidget()
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QGridLayout()
+        self.setLayout(layout)
 
-windows.resize(500,500)
-windows.move(100,100)
-windows.setWindowTitle('Editor')
-windows.setWindowIcon(QtGui.QIcon('images/icon.png'))
-windows.show()
-sys.exit(app.exec_())
+        menubar = QtWidgets.QMenuBar()
+        layout.addWidget(menubar, 0, 0)
+
+        actionFile = menubar.addMenu("File")
+        actionFile.addAction("Open")
+        actionFile.addAction("Save")
+        actionFile.addSeparator()
+        actionFile.addAction("Quit", self.close)
+
+        menubar.addMenu("Help")
+
+        self.resize(500, 500)
+        self.move(100, 100)
+        self.setWindowTitle('Editor')
+        self.setWindowIcon(QtGui.QIcon('images/icon.png'))
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
